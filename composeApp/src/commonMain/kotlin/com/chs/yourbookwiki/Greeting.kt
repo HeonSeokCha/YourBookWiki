2202da30334252d00f68a842b6351e7867ea3208
hyeonseok <hs960829@gmail.com>
2024-11-18 21:07:41 +0900

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/chs/yourbookwiki/Greeting.kt
@@ -0,0 +1,9 @@
+package com.chs.yourbookwiki
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
